fb9ab097e2488d2014f9815f8e0bf065a3d7fc2d
pgaulon <pierre.gaulon1@orange.fr>
2015-08-28 14:38:03 +0800

    correcting tests

--- testing/btest/Traces/enip/script.sh
+++ testing/btest/Traces/enip/script.sh
@@ -3,7 +3,7 @@
 protocol=enip
 extension=pcapng
 
-test="# @TEST-EXEC: btest-diff output\n# @TEST-EXEC: cat output | awk '{print $1}' | sort | uniq | wc -l > covered\n# @TEST-EXEC: cat \${DIST}/src/analyzer/protocol/$protocol/events.bif | grep \"^event "$protocol"_\" | wc -l > total\n# @TEST-EXEC: echo \`cat covered\` of \`cat total\` events triggered by trace > coverage\n# @TEST-EXEC: btest-diff coverage\n# @TEST-EXEC: btest-diff $protocol.log\n#"
+test="# @TEST-EXEC: btest-diff output\n# @TEST-EXEC: cat output | awk '{print \$1}' | sort | uniq | wc -l > covered\n# @TEST-EXEC: cat \${DIST}/src/analyzer/protocol/$protocol/events.bif | grep \"^event "$protocol"_\" | wc -l > total\n# @TEST-EXEC: echo \`cat covered\` of \`cat total\` events triggered by trace > coverage\n# @TEST-EXEC: btest-diff coverage\n# @TEST-EXEC: btest-diff $protocol.log\n#"
 
 for i in $( ls ); do
     if [[ $i = *."$extension" ]]; then
--- testing/btest/scripts/base/protocols/enip/enip_upload_plc1.bro
+++ testing/btest/scripts/base/protocols/enip/enip_upload_plc1.bro
@@ -1,7 +1,7 @@
 #
 # @TEST-EXEC: bro -r $TRACES/enip/enip_upload_plc1.pcapng %DIR/events.bro > output
 # @TEST-EXEC: btest-diff output
-# @TEST-EXEC: cat output | awk '{print }' | sort | uniq | wc -l > covered
+# @TEST-EXEC: cat output | awk '{print $1}' | sort | uniq | wc -l > covered
 # @TEST-EXEC: cat ${DIST}/src/analyzer/protocol/enip/events.bif | grep "^event enip_" | wc -l > total
 # @TEST-EXEC: echo `cat covered` of `cat total` events triggered by trace > coverage
 # @TEST-EXEC: btest-diff coverage
